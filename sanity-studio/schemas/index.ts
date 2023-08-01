c0e0d340634cd375d51d4561efa27615a8ed0ecf
retz8 <ekkicb71@gmail.com>
2023-08-02 01:20:24 +0900

    clear git cache

--- sanity-studio/schemas/index.ts
+++
@@ -1,9 +0,0 @@
-import eggRemoval from './eggRemoval'
-import faq from './faq'
-import generalInfo from './generalInfo'
-import home from './home'
-import lfTraps from './lfTraps'
-import member from './member'
-import schedule from './schedule'
-
-export const schemaTypes = [faq, member, generalInfo, schedule, eggRemoval, lfTraps, home]
